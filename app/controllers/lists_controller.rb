1436c054313cb4d8feb256c875676bf5c171d885
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-07-06 15:31:40 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,7 @@ class ListsController < ApplicationController
     
     list.save
     
-    redirect_to'/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -18,6 +18,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list=List.find(params[:id])
   end
 
   def edit
