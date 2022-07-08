dfe3ed9bbf5152020d38bea08cdd6ea6dfea1404
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-07-08 14:06:30 +0000

    [Finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,13 +4,15 @@ class ListsController < ApplicationController
   end
   
   def create
-    list=List.new(list_params)
-    
-    list.save
-    
-    redirect_to list_path(list.id)
+    @list=List.new(list_params)
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render:new
+    end
   end
-
+    
+ 
   def index
     @lists = List.all  
     
@@ -43,5 +45,6 @@ class ListsController < ApplicationController
   def list_params
     params.require(:list).permit(:title,:body,:image)
   end
-  
 end
+  
+  
